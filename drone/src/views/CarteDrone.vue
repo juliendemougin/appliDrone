2e5329b25fe2b688970e33168eb2702e11c7c7a0
guillaume Houllé <guillaumehoulle@gmail.com>
2022-04-07 11:35:45 +0200

    SCSS

--- drone/src/views/CarteDrone.vue
+++ drone/src/views/CarteDrone.vue
@@ -1,6 +1,25 @@
 <template>
     <ion-content>
         sfd
+    <ion-footer class="footer">
+        <!-- <footer class="footer"> -->
+        <div class="footer__border footer__border--bord">
+            <router-link to="documents.html">
+                <img class="footer__icon" src="../icons/footer/icon_folder.svg" alt="image d'un dossier">
+            </router-link>
+        </div>
+        <div class="footer__border footer__border--bord">
+            <a href="map.html">
+                <img class="footer__icon" src="../icons/footer/icon_map.svg" alt="image d'un carte">
+            </a>
+        </div>
+        <div class="footer__border">
+            <a href="meteo.html">
+                <img class="footer__icon" src="../icons/footer/icon_weather.svg" alt="image d'un nuage">
+            </a>
+        </div>
+    <!-- </footer> -->
+    </ion-footer>
     </ion-content>
 </template>
 
