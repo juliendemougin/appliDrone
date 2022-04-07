8755662ccb24d64535477ad46a83b010026ffa05
guillaume Houllé <guillaumehoulle@gmail.com>
2022-04-07 16:26:24 +0200

    Maj Map

--- drone/src/views/CarteDrone.vue
+++ drone/src/views/CarteDrone.vue
@@ -1,32 +1,48 @@
 <template>
-    <ion-content>
-        sfd
-    <ion-footer class="footer">
-        <!-- <footer class="footer"> -->
-        <div class="footer__border footer__border--bord">
-            <router-link to="/documents">
-                <img class="footer__icon" src="../icons/footer/icon_folder.svg" alt="image d'un dossier">
-            </router-link>
-        </div>
-        <div class="footer__border footer__border--bord">
-            <router-link to="/cartedrone">
-                <img class="footer__icon" src="../icons/footer/icon_map.svg" alt="image d'un carte">
-            </router-link>
-        </div>
-        <div class="footer__border">
-            <router-link to="/meteo">
-                <img class="footer__icon" src="../icons/footer/icon_weather.svg" alt="image d'un nuage">
-            </router-link>
-        </div>
-    <!-- </footer> -->
-    </ion-footer>
-    </ion-content>
+    <ion-page>
+        <ion-content>
+            sfd
+        </ion-content>
+        <ion-footer class="footer">
+            <!-- <footer class="footer"> -->
+            <div class="footer__border footer__border--bord">
+                <router-link to="/documents">
+                    <img class="footer__icon" src="../icons/footer/icon_folder.svg" alt="image d'un dossier">
+                </router-link>
+            </div>
+            <div class="footer__border footer__border--bord">
+                <router-link to="/cartedrone">
+                    <img class="footer__icon" src="../icons/footer/icon_map.svg" alt="image d'un carte">
+                </router-link>
+            </div>
+            <div class="footer__border">
+                <router-link to="/meteo">
+                    <img class="footer__icon" src="../icons/footer/icon_weather.svg" alt="image d'un nuage">
+                </router-link>
+            </div>
+        <!-- </footer> -->
+        </ion-footer>
+        
+    </ion-page>
 </template>
 
 <script>
 // import { defineComponent } from '@vue/composition-api'
 
-export default ({
+  import { useRouter } from 'vue-router';
+  import { defineComponent } from 'vue';
+  import { IonPage, IonContent } from '@ionic/vue';
+
+  export default defineComponent({
     name: 'CarteDrone',
-})
+    components: {
+      IonPage,
+      IonContent
+    },
+    setup() {
+      return {
+        router: useRouter()
+      }
+    }
+  });
 </script>
