b90f20f6e9808e3a145d7d8f8c886190513e7caa
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-08 10:07:12 +0200

    fonction logout

--- drone/src/views/HomePage.vue
+++ drone/src/views/HomePage.vue
@@ -2,10 +2,10 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title style="color: black;">DRONE</ion-title>
+        <ion-title style="color: black">DRONE</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
@@ -15,7 +15,16 @@
       <!-- <LeHeader></LeHeader> -->
       <div id="container">
         <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
+        <ion-button @click="doLogout">Se déconnecter</ion-button>
       </div>
     </ion-content>
   </ion-page>
@@ -23,12 +32,20 @@
 
 <script lang="ts">
 // import { IonContent, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import useFirebaseAuth from "../hooks/firebase-auth";
 // import LeHeader from '../components/LeHeader.vue';
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonPage,
@@ -36,10 +53,21 @@ export default defineComponent({
     IonTitle,
     IonToolbar,
     // LeHeader
-  }
+  },
+
+  setup() {
+    const { logout } = useFirebaseAuth();
+    const router = useRouter();
+    const doLogout = () => {
+      router.replace({ path: "/login" });
+    };
+
+    return {
+      doLogout,
+    };
+  },
 });
 </script>
 
 <style >
-
 </style>
