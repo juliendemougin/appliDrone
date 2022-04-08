0cd698463205004be36bde1f04a049349578f795
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-08 10:06:33 +0200

    Authentification firebase

---
+++ drone/src/hooks/.env.firebase.ts
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDYVIDxqI5j2bJ0vYa8Ko5DyhzxpLYKY5s",
+    authDomain: "drone-test-aaee2.firebaseapp.com",
+    projectId: "drone-test-aaee2",
+    storageBucket: "drone-test-aaee2.appspot.com",
+    messagingSenderId: "935075277193",
+    appId: "1:935075277193:web:ddf2d16eea1b2bb09ed958"
+};
+
+export default FIREBASE_CONFIG;
