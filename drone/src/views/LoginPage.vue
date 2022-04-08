8aa0d1a48d5de676bfa18beb8af186b11569e5f2
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-08 10:07:26 +0200

    fonction login

--- drone/src/views/LoginPage.vue
+++ drone/src/views/LoginPage.vue
@@ -1,47 +1,82 @@
 <template>
-    <main class="container">
-        <form action="#">
-            <div class="connexion">
-                <div class="connexion__mail mail">
-                    <input class="input input--email" type="text" placeholder="adresse e-mail" required>
-                </div>
-                <div class="connexion__password password">
-                    <input class="input input--password" type="password" placeholder="mot de passe" required>
-                </div>
-                <div class="connexion__oubli__container">
-                    <a class="connexion__oubli" href="#">Mot de passe oublié ?</a>
-                </div>
-                <div class="connexion__checkbox">
-                    <label class="b-contain">
-                        <span>Rester connecté</span>
-                        <input type="checkbox">
-                        <div class="b-input"></div>
-                    </label>
-                </div>
-                <input class="lg_button connexion__button" type="submit" value="Se connecter">
-                <ion-button @click="router.replace({path:'/home'})">Se connecter</ion-button>
-                <div class="connexion__ou__container">
-                    <p class="connexion__ou">OU</p>
-                    <span class="connexion__right-line"></span>
-                </div>
-                <div class="connexion__sso">
-                    <a class="connexion__sso__google" href="#"><img src="../icons/icon_google.svg" alt="icône google"></a>
-                    <a class="connexion__sso__facebook" href="#"><img src="../icons/icon_facebook.svg" alt="icône facebook"></a>
-                </div>
-            </div>
-        </form>
-    </main>
+    <ion-page>
+        <ion-content class="ion-padding">
+            <ion-item>
+                <ion-input name="username" type="text" placeholder="adresse email" @ionChange="handleChange" />
+            </ion-item>
+            <ion-item>
+                <ion-input name="password" type="password" placeholder="mot de passe" @ionChange="handleChange" />
+            </ion-item>
+            <ion-button @click="doLogin">Se connecter</ion-button>
+        </ion-content>
+    </ion-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
-import { useRouter } from 'vue-router';
 
+<script lang="ts">
+import useFirebaseAuth from "../hooks/firebase-auth";
+import { defineComponent, ref } from "vue";
+import {
+  IonContent,
+  IonPage,
+  alertController,
+  IonInput,
+  IonButton,
+  IonItem,
+} from "@ionic/vue";
+import { useRouter } from "vue-router";
 export default defineComponent({
-    setup() {
-        return {
-            router: useRouter(),
-        };
-    },
+  components: {
+    IonContent,
+    IonPage,
+    IonInput,
+    IonButton,
+    IonItem,
+  },
+  setup() {
+    const state = useFirebaseAuth();
+    const router = useRouter();
+    const credentials = ref<{ [key: string]: string }>({
+      username: "",
+      password: "",
+    });
+    const handleChange = (e: CustomEvent) => {
+      console.log(e);
+      const name: string = (e?.target as any)?.name;
+      credentials.value[name as string] = e.detail.value;
+    };
+    /**
+     *
+     */
+    const handleAlert = (message: string, isError = false) => {
+      alertController
+        .create({
+          header: isError ? "Error Message" : "Notice",
+          message: message,
+          buttons: ["OK"],
+        })
+        .then((t) => t.present());
+    };
+    /**
+     *
+     *
+     */
+    const doLogin = async () => {
+      try {
+        const { username, password } = credentials.value;
+        await state.login(username, password);
+        router.push({path : "/home", replace: true });
+      } catch (error) {
+        console.log(error);
+        handleAlert(error.message, true);
+      }
+    };
+    return {
+      ...state,
+      credentials,
+      doLogin,
+      handleChange,
+    };
+  },
 });
 </script>
