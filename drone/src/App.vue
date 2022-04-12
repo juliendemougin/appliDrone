3077957f29a4297bfe2c284994ff542c4fd468bd
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-12 10:21:08 +0200

    Ajout de la fonction closeNav

--- drone/src/App.vue
+++ drone/src/App.vue
@@ -2,28 +2,28 @@
   <ion-app>
     <ion-header>
         <div id="nav" class="nav">
-          <router-link id="closeBtn" to="#" class="nav__close"><img src="./icons/icon_cross.svg" alt="icône fermer"></router-link>
+          <router-link to="#" id="closeBtn" class="nav__close"><img src="./icons/icon_cross.svg" alt="icône fermer"></router-link>
           <ul class="nav__list">
               <li class="nav__item">
-                <router-link class="nav__link" to="/profil">Profil</router-link>
+                <router-link class="nav__link" to="/profil" onclick="closeNav();">Profil</router-link>
               </li>
               <li class="nav__item">
-                <router-link class="nav__link" to="/documents">Documents</router-link>
+                <router-link class="nav__link" to="/documents" onclick="closeNav();">Documents</router-link>
               </li>
               <li class="nav__item">
-                <router-link class="nav__link" to="/cartedrone">Carte</router-link>
+                <router-link class="nav__link" to="/cartedrone" onclick="closeNav();">Carte</router-link>
               </li>
               <li class="nav__item">
-                <router-link class="nav__link" to="/liens">Documentation</router-link>
+                <router-link class="nav__link" to="/liens" onclick="closeNav();">Documentation</router-link>
               </li>
               <li class="nav__item">
-                <router-link class="nav__link" to="/abo">S'abonner</router-link>
+                <router-link class="nav__link" to="/abo" onclick="closeNav();">S'abonner</router-link>
               </li>
               <li class="nav__item">
-                <router-link class="nav__link" to="#">Deconnexion</router-link>
+                <router-link class="nav__link" to="#" onclick="closeNav();">Deconnexion</router-link>
               </li>
               <li class="nav__item">
-                <router-link class="nav__link" to="/contact">Contact</router-link>
+                <router-link class="nav__link" to="/contact" onclick="closeNav();">Contact</router-link>
               </li>
           </ul>
         </div>
@@ -46,9 +46,6 @@
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
-
-
-
 export default defineComponent({
   name: 'App',
   components: {
