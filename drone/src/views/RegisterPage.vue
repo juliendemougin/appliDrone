3c559bc1b593922fd0a1787639989a8b3f4cf511
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-12 10:25:10 +0200

    Fonction d'inscription

--- drone/src/views/RegisterPage.vue
+++ drone/src/views/RegisterPage.vue
@@ -1,17 +1,17 @@
 <template>
     <main class="container">
-        <form action="#">
+        <form @submit.prevent="register">
             <div class="inscription">
-                <input class="input" type="text" placeholder="Pseudo *" size="20" required>
-                <input class="input" type="email" placeholder="Adresse e-mail *" required>
-                <input class="input" type="text" placeholder="Prenom *" required>
-                <input class="input" type="text" placeholder="Nom *" required>
-                <input class="input" type="password" placeholder="Mot de passe *" required>
-                <input class="input" type="password" placeholder="Confirmation *" required>
+                <input class="input" type="text" placeholder="Pseudo *" size="20">
+                <input class="input" v-model="email" type="email" placeholder="Adresse e-mail *" required>
+                <input class="input" type="text" placeholder="Prenom *">
+                <input class="input" type="text" placeholder="Nom *">
+                <input class="input"  v-model="password" type="password" placeholder="Mot de passe *" required>
+                <input class="input" type="password" placeholder="Confirmation *">
                 <div class="inscription__checkbox">
                     <label class="b-contain">
                         <span>J'accepte les <router-link class="inscription__link" to="conditions">conditions d'utilisation</router-link></span>
-                        <input type="checkbox" required>
+                        <input type="checkbox">
                         <div class="b-input"></div>
                     </label>
                 </div>
@@ -20,3 +20,23 @@
         </form>
     </main>
 </template>
+
+<script>
+import firebase from 'firebase/compat/app';
+
+export default {
+    methods: {
+        register () {
+            firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.email, this.password)
+            .then(() => {
+                this.$router.push('/login');
+            })
+            .catch(error => {
+                alert(error.message);
+            });
+        }
+    }
+}
+</script>
