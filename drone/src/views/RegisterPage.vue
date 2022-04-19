4fd18736575982b289e4771d7778df035c9cd111
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-19 10:58:29 +0200

    Fonction d'inscription

--- drone/src/views/RegisterPage.vue
+++ drone/src/views/RegisterPage.vue
@@ -2,12 +2,10 @@
     <main class="container">
         <form @submit.prevent="register">
             <div class="inscription">
-                <input class="input" type="text" placeholder="Pseudo *" size="20">
+                <input class="input" v-model="pseudo" type="text" placeholder="Pseudo *" size="20">
                 <input class="input" v-model="email" type="email" placeholder="Adresse e-mail *" required>
-                <input class="input" type="text" placeholder="Prenom *">
-                <input class="input" type="text" placeholder="Nom *">
-                <input class="input"  v-model="password" type="password" placeholder="Mot de passe *" required>
-                <input class="input" type="password" placeholder="Confirmation *">
+                <input class="input" v-model="password" type="password" placeholder="Mot de passe *" required>
+                <input class="input" v-model="validation" type="password" placeholder="Confirmation *">
                 <div class="inscription__checkbox">
                     <label class="b-contain">
                         <span>J'accepte les <router-link class="inscription__link" to="conditions">conditions d'utilisation</router-link></span>
@@ -21,21 +19,26 @@
     </main>
 </template>
 
-<script>
+<script lang="ts">
 import firebase from 'firebase/compat/app';
 
 export default {
     methods: {
         register () {
-            firebase
-            .auth()
-            .createUserWithEmailAndPassword(this.email, this.password)
-            .then(() => {
-                this.$router.push('/login');
-            })
-            .catch(error => {
-                alert(error.message);
-            });
+            if(this.password != this.validation) {
+                alert('Les mots de passe ne correspondent pas');
+            } else {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+                .then(user => {
+                    return user.user.updateProfile({
+                        displayName: this.pseudo
+                    })
+                })
+                .then(() => {
+                    this.$router.push('/login');
+                    console.log(firebase.auth().currentUser.displayName);
+                })
+            }
         }
     }
 }
