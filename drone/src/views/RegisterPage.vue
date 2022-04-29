0b6016753fd6caacda28f51d0a90c508f5dcfbf4
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-04-29 09:48:26 +0200

    Fonction de création de compte en base de donnée

--- drone/src/views/RegisterPage.vue
+++ drone/src/views/RegisterPage.vue
@@ -2,7 +2,9 @@
     <main class="container">
         <form @submit.prevent="register">
             <div class="inscription">
-                <input class="input" v-model="pseudo" type="text" placeholder="Pseudo *" size="20">
+                <input class="input" v-model="pseudo" type="text" placeholder="Pseudo *" size="20" required>
+                <input class="input" v-model="prenom" type="text" placeholder="Prénom *" required>
+                <input class="input" v-model="nom" type="text" placeholder="Nom *" required>
                 <input class="input" v-model="email" type="email" placeholder="Adresse e-mail *" required>
                 <input class="input" v-model="password" type="password" placeholder="Mot de passe *" required>
                 <input class="input" v-model="validation" type="password" placeholder="Confirmation *">
@@ -19,24 +21,62 @@
     </main>
 </template>
 
-<script lang="ts">
+<script>
 import firebase from 'firebase/compat/app';
+import axios from 'axios';
 
 export default {
+    data() {
+        return {
+            pseudo: '',
+            prenom: '',
+            nom: '',
+            email: '',
+            errorMessage: '',
+            token: ''
+        }
+    },
+
     methods: {
         register () {
+            const getId = async () => {
+                const user = firebase.auth().currentUser;
+                if(user) {
+                    const token = await user.getIdToken(true);
+                    console.log(`Token: ${token}`)
+                    this.token = token;
+                    return this.token;
+                }
+            }
+
             if(this.password != this.validation) {
                 alert('Les mots de passe ne correspondent pas');
             } else {
                 firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
                 .then(user => {
+                    getId();
                     return user.user.updateProfile({
                         displayName: this.pseudo
                     })
                 })
                 .then(() => {
-                    this.$router.push('/login');
-                    console.log(firebase.auth().currentUser.displayName);
+                    if(this.token === "") {
+                        throw new Exception ("Une erreur s'est produite lors de la création de votre compte, veuillez réessayer.")
+                    } else {
+                        var form = new FormData();
+                        form.append('pseudo', this.pseudo);
+                        form.append('nom', this.nom);
+                        form.append('prenom', this.prenom);
+                        form.append('email', this.email);
+                        form.append('fid', this.token);
+                        axios.post('https://nervous-euclid.82-165-48-211.plesk.page/adduser', form);
+                        this.$router.push('/login');
+                        console.log(firebase.auth().currentUser.displayName);
+                    }
+                })
+                .catch(error => {
+                    this.errorMessage = error.message;
+                    console.error("There was an error!", error);
                 })
             }
         }
