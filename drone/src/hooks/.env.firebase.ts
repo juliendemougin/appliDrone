8dfd6c132deb3e94fb217abd4d2ed6fec7d5ee2f
A-Berthe <92781217+A-Berthe@users.noreply.github.com>
2022-05-09 14:24:44 +0200

    Mise à jour config firebase

--- drone/src/hooks/.env.firebase.ts
+++ drone/src/hooks/.env.firebase.ts
@@ -1,10 +1,11 @@
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDYVIDxqI5j2bJ0vYa8Ko5DyhzxpLYKY5s",
-    authDomain: "drone-test-aaee2.firebaseapp.com",
-    projectId: "drone-test-aaee2",
-    storageBucket: "drone-test-aaee2.appspot.com",
-    messagingSenderId: "935075277193",
-    appId: "1:935075277193:web:ddf2d16eea1b2bb09ed958"
+    apiKey: "AIzaSyDNiMOpD9z6kOMhmblHnhmhXCjWXWyykF8",
+    authDomain: "drone-261c6.firebaseapp.com",
+    projectId: "drone-261c6",
+    storageBucket: "drone-261c6.appspot.com",
+    messagingSenderId: "112169540211",
+    appId: "1:112169540211:web:f443893f625028d6c2e806",
+    measurementId: "G-897W0QD0Z1"
 };
 
 export default FIREBASE_CONFIG;
